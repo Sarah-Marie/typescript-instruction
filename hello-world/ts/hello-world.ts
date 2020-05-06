37b2294dd87563fb39ae1ad026c2fd31d293e0f4
Sarah-Marie <sarwmsn@gmail.com>
2020-05-06 13:48:06 -0400

    init commit

---
+++ hello-world/ts/hello-world.ts
@@ -0,0 +1,3 @@
+let hw:string = "Hello, world";
+let person:string = "student";
+console.log(hw, person);
---
+++ temp/hello-test.ts
@@ -0,0 +1,5 @@
+//how to define a variable in javascript (variable a assigned to hello)
+ let a = "hello";
+ let b = 5;
+//print out
+console.log(a + "" + b);
